6b628971e247057aa6fd4fe0fc87d8b565d02d8b
Emerson <emerson.matosokl10@gmail.com>
2023-03-25 20:25:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Nome do Projeto
+Escreva o nome do seu projeto.
+
+Descrição
+Escreva uma breve descrição do seu projeto e o problema que ele resolve.
+
+Tecnologias
+ HTML, 
+ CSS, 
+ JavaScript
+
---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+/*=============== SHOW SIDEBAR ===============*/
+
+
+/*===== SIDEBAR SHOW =====*/
+/* Validate If Constant Exists */
+
+
+/*===== SIDEBAR HIDDEN =====*/
+/* Validate If Constant Exists */
+
+
+/*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContent = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContent.forEach(tabContents => {
+            tabContents.classList.remove('skills__active')
+        })
+
+        target.classList.add('skills__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('skills__active')
+        })
+
+        tab.classList.add('skills__active')
+    })
+})
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+
+/*===== Link Active Work =====*/
+
+
+/*===== Work Popup =====*/
+
+
+/*=============== SERVICES MODAL ===============*/
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== INPUT ANIMATION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/
