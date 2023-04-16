9e73ae4e2a356677cd858aa683829707b4836239
Emerson <emerson.matosokl10@gmail.com>
2023-04-16 18:33:52 -0300

    Commit: 3a4c8f4e2f1e1029687d90c07b8d1b632d87451d Author: emersonSantos98 Date: 16/04/2023
    Foram adicionados os seguintes arquivos:
    
    mixitup.min.js
    Arquivos alterados:
    
    index.html
    assets/css
    assets/js
    Este commit adiciona uma nova seção de trabalhos recentes na página inicial, com filtros para visualização dos projetos por categoria: todos, web, api e integração.
    
    Assinado-off-by: emersonSantos98

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,10 +32,23 @@ tabs.forEach(tab => {
 })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /*===== Link Active Work =====*/
+const linwork = document.querySelectorAll('.work__item')
 
+function activeLink() {
+    linwork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+linwork.forEach(l => l.addEventListener('click', activeLink))
 
 /*===== Work Popup =====*/
 
