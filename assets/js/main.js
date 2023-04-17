6948385699411accd14778a601aca0b56f6e5b85
Emerson <emerson.matosokl10@gmail.com>
2023-04-17 09:53:33 -0300

    Commit: 3a4c8f4e2f1e1029687d90c07b8d1b632d87451d Author: emersonSantos98 Date: 17/04/2023
    Arquivos alterados:
    index.html
    assets/css
    assets/js
    Este commit adiciona uma nova modal para visualizar as categorias.
    
    Assinado-off-by: emersonSantos98

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,24 @@ function activeLink() {
 linwork.forEach(l => l.addEventListener('click', activeLink))
 
 /*===== Work Popup =====*/
+document.addEventListener('click', (e) => {
+ if(e.target.classList.contains("work__button")){
+     togglePortfolioPopup();
+     portfolioItemDetails(e.target.parentElement);
+ }
+})
 
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio_popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio_popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
 
 /*=============== SERVICES MODAL ===============*/
 
